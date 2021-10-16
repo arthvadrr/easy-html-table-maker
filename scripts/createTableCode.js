19ac6f6a51283c1419c9283e9cafa69cf5bb7200
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-10-16 00:33:25 -0500

    Refactored createTableCode.js

--- scripts/createTableCode.js
+++ scripts/createTableCode.js
@@ -1,52 +1,42 @@
 const createTableCode = state => {
     const $code_tableCode = document.getElementById('table-code');
+    let output = '';
+    const indent = amount => '\u00A0'.repeat(amount);
 
     for (let t = 0; t < state.tables.length; t++) {
-        createTextNode('<table>', $code_tableCode, true);
+        output += '&lt;table&gt<br>';
 
         if (state.tables[t].header) {
-            createTextNode('<thead>', $code_tableCode, true, 2);
-            createTextNode('<tr>', $code_tableCode, true, 4);
+            output += `
+            ${indent(2)}&lt;thead&gt<br>
+            ${indent(4)}&lt;tr&gt<br>
+            `;
             for (let h = 0; h < state.tables[t].columns; h++) {
-                createTextNode('<th>', $code_tableCode, false, 6);
-                createTextNode(state.tables[t].headerContent[h].innerHTML, $code_tableCode);
-                createTextNode('</th>', $code_tableCode, true);
+                output += `${indent(6)}&lt;th&gt${
+                    state.tables[t].headerContent[h].innerHTML
+                }&lt;/th&gt<br>`;
             }
-            createTextNode('</tr>', $code_tableCode, true, 4);
-            createTextNode('</thead>', $code_tableCode, true, 2);
+            output += `
+            ${indent(4)}&lt;/tr&gt<br>
+            ${indent(2)}&lt;/thead&gt<br>
+            `;
         }
 
-        createTextNode('<tbody>', $code_tableCode, true, 2);
+        output += `${indent(2)}&lt;tbody&gt<br>`;
         for (let i = 0; i < state.tables[t].rows; i++) {
-            createTextNode('<tr>', $code_tableCode, true, 4);
+            output += `${indent(4)}&lt;tr&gt<br>`;
 
             for (let p = 0; p < state.tables[t].columns; p++) {
-                createTextNode('<td>', $code_tableCode, false, 6);
-                createTextNode(state.tables[t].content[i][p].innerHTML, $code_tableCode);
-                createTextNode('</td>', $code_tableCode, true);
+                output += `${indent(6)}&lt;td&gt${
+                    state.tables[t].content[i][p].innerHTML
+                }&lt;/td&gt<br>`;
             }
-
-            createTextNode('</tr>', $code_tableCode, true, 4);
+            output += `${indent(4)}&lt;/tr&gt<br>`;
         }
-        createTextNode('</tbody>', $code_tableCode, true, 2);
-        createTextNode('</table>', $code_tableCode, true);
+        output += `${indent(2)}&lt;/tbody&gt<br>`;
+        output += `&lt;/table&gt<br>`;
     }
-};
-
-const createTextNode = (text, parent, lineBreak, indentAmount = 0) => {
-    const textNode = document.createTextNode(text);
-
-    for (let i = 0; i < indentAmount; i++) {
-        const indentNode = document.createTextNode('\u00A0');
-        parent.appendChild(indentNode);
-    }
-
-    parent.appendChild(textNode);
-
-    if (lineBreak) {
-        const lineBreakElement = document.createElement('br');
-        parent.appendChild(lineBreakElement);
-    }
+    $code_tableCode.innerHTML = output;
 };
 
 export default createTableCode;
