c3fad1cbf6f40cb7e4157134780b999cd641fcdc
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-10-24 22:54:09 -0500

    Removed multi table

--- scripts/createEditorTable.js
+++ scripts/createEditorTable.js
@@ -18,213 +18,198 @@ const createEditorTable = StateMachine => {
         createEditorTable(StateMachine, $div_editorTableContainer, $code_tableCode);
     };
 
-    state.tables.map((table, index) => {
-        createElement('div', $div_editorTableContainer, `editor-table-container-${index}`);
-        createElement(
-            'table',
-            `editor-table-container-${index}`,
-            `editor-table-${index}`,
-            'editor-table'
-        );
-        createElement(
-            'button',
-            `editor-table-container-${index}`,
-            `add-row-${index}`,
-            'add-row-button',
-            false,
-            'Add Row',
-            {
-                type: 'click',
-                func: () => {
-                    state.tables[index].rows++;
-                    state.tables[index].content.push(createTableRow(StateMachine.state, index));
-                    refresh();
-                },
-            }
-        );
-        createElement(
-            'button',
-            `editor-table-container-${index}`,
-            `remove-row-${index}`,
-            'remove-row-button',
-            false,
-            'Remove Row',
-            {
-                type: 'click',
-                func: () => {
-                    if (state.tables[index].rows > 0) {
-                        state.tables[index].rows--;
-                    }
-                    state.tables[index].content.pop();
-                    refresh();
-                },
-            }
-        );
-        createElement(
-            'button',
-            `editor-table-container-${index}`,
-            `add-column-${index}`,
-            'add-column-button',
-            false,
-            'Add Column',
-            {
-                type: 'click',
-                func: () => {
-                    const col = { innerHTML: 'col' };
-
-                    if (table.header) {
-                        state.tables[index].headerContent.push(col);
-                    }
-
-                    state.tables[index].content.forEach(element => {
-                        element.push(col);
-                    });
-                    state.tables[index].columns++;
-                    refresh();
-                },
-            }
-        );
-        createElement(
-            'button',
-            `editor-table-container-${index}`,
-            `remove-column-${index}`,
-            'remove-column-button',
-            false,
-            'Remove Column',
-            {
-                type: 'click',
-                func: () => {
-                    const col = { innerHTML: 'col' };
-
-                    if (table.header) {
-                        state.tables[index].headerContent.pop();
-                    }
-
-                    state.tables[index].content.forEach(element => {
-                        element.pop();
-                    });
-                    if (state.tables[index].columns > 0) {
-                        state.tables[index].columns--;
-                    }
-                    refresh();
-                },
-            }
-        );
-        createElement(
-            'input',
-            `editor-table-container-${index}`,
-            `caption-toggle-${index}`,
-            'caption-toggle',
-            false,
-            false,
-            {
-                type: 'click',
-                func: e => {
-                    state.tables[index].caption = !state.tables[index].caption;
-                    if (e.target.checked) {
-                        console.log('checked');
-                    } else {
-                        console.log('not checked');
-                    }
-                    refresh();
-                },
+    createElement('div', $div_editorTableContainer, `editor-table-container`);
+    createElement('table', `editor-table-container`, `editor-table`, 'editor-table');
+    createElement(
+        'button',
+        `editor-table-container`,
+        `add-row`,
+        'add-row-button',
+        false,
+        'Add Row',
+        {
+            type: 'click',
+            func: () => {
+                state.rows++;
+                state.content.push(createTableRow(StateMachine.state, index));
+                refresh();
             },
-            {
-                type: 'checkbox',
-                name: 'captionToggle',
-                value: 'Toggle caption',
-                checked: state.tables[index].caption,
-            }
-        );
-
-        createElement(
-            'input',
-            `editor-table-container-${index}`,
-            `header-toggle-${index}`,
-            'header-toggle',
-            false,
-            false,
-            {
-                type: 'click',
-                func: () => {
-                    state.tables[index].header = !state.tables[index].header;
-                    refresh();
-                },
+        }
+    );
+    createElement(
+        'button',
+        `editor-table-container`,
+        `remove-row`,
+        'remove-row-button',
+        false,
+        'Remove Row',
+        {
+            type: 'click',
+            func: () => {
+                if (state.rows > 0) {
+                    state.rows--;
+                }
+                state.content.pop();
+                refresh();
             },
-            {
-                type: 'checkbox',
-                name: 'headerToggle',
-                value: 'Table header',
-                checked: state.tables[index].header,
-            }
-        );
+        }
+    );
+    createElement(
+        'button',
+        `editor-table-container`,
+        `add-column`,
+        'add-column-button',
+        false,
+        'Add Column',
+        {
+            type: 'click',
+            func: () => {
+                const col = { innerHTML: 'col' };
 
-        if (table.caption) {
+                if (state.header) {
+                    state.headerContent.push(col);
+                }
+
+                state.content.forEach(element => {
+                    element.push(col);
+                });
+                state.columns++;
+                refresh();
+            },
+        }
+    );
+    createElement(
+        'button',
+        `editor-table-container`,
+        `remove-column`,
+        'remove-column-button',
+        false,
+        'Remove Column',
+        {
+            type: 'click',
+            func: () => {
+                const col = { innerHTML: 'col' };
+
+                if (state.header) {
+                    state.headerContent.pop();
+                }
+
+                state.content.forEach(element => {
+                    element.pop();
+                });
+                if (state.columns > 0) {
+                    state.columns--;
+                }
+                refresh();
+            },
+        }
+    );
+    createElement(
+        'input',
+        `editor-table-container`,
+        `caption-toggle`,
+        'caption-toggle',
+        false,
+        false,
+        {
+            type: 'click',
+            func: e => {
+                state.caption = !state.caption;
+                if (e.target.checked) {
+                    console.log('checked');
+                } else {
+                    console.log('not checked');
+                }
+                refresh();
+            },
+        },
+        {
+            type: 'checkbox',
+            name: 'captionToggle',
+            value: 'Toggle caption',
+            checked: state.caption,
+        }
+    );
+
+    createElement(
+        'input',
+        `editor-table-container`,
+        `header-toggle`,
+        'header-toggle',
+        false,
+        false,
+        {
+            type: 'click',
+            func: () => {
+                state.header = !state.header;
+                refresh();
+            },
+        },
+        {
+            type: 'checkbox',
+            name: 'headerToggle',
+            value: 'Table header',
+            checked: state.header,
+        }
+    );
+
+    if (state.caption) {
+        createElement('caption', `editor-table`, `table-caption`, false, true, state.captionText, {
+            type: 'input',
+            func: e => {
+                state.captionText = e.target.textContent;
+                localStorage.setItem('savedState', JSON.stringify(state));
+                createTableCode(StateMachine.state, $code_tableCode);
+            },
+        });
+    }
+
+    if (state.header) {
+        createElement('thead', `editor-table`, `table-head`);
+        createElement('tr', `table-head`, `table-header`);
+        for (let h = 0; h < state.columns; h++) {
             createElement(
-                'caption',
-                `editor-table-${index}`,
-                `table-caption-${index}`,
+                'th',
+                `table-header`,
+                false,
                 false,
                 true,
-                table.captionText,
+                state.headerContent[h].innerHTML,
                 {
                     type: 'input',
                     func: e => {
-                        state.tables[index].captionText = e.target.textContent;
+                        state.headerContent[h].innerHTML = e.target.textContent;
                         localStorage.setItem('savedState', JSON.stringify(state));
                         createTableCode(StateMachine.state, $code_tableCode);
                     },
                 }
             );
         }
+    }
 
-        if (table.header) {
-            createElement('thead', `editor-table-${index}`, `table-head-${index}`);
-            createElement('tr', `table-head-${index}`, `table-header-${index}`);
-            for (let h = 0; h < table.columns; h++) {
-                createElement(
-                    'th',
-                    `table-header-${index}`,
-                    false,
-                    false,
-                    true,
-                    table.headerContent[h].innerHTML,
-                    {
-                        type: 'input',
-                        func: e => {
-                            state.tables[index].headerContent[h].innerHTML = e.target.textContent;
-                            localStorage.setItem('savedState', JSON.stringify(state));
-                            createTableCode(StateMachine.state, $code_tableCode);
-                        },
-                    }
-                );
-            }
+    for (let r = 0; r < state.rows; r++) {
+        createElement('tbody', `editor-table`, `table-body`);
+        createElement('tr', `table-body`, `table-row-${r}`);
+
+        for (let c = 0; c < state.columns; c++) {
+            createElement(
+                'td',
+                `table-row-${r}`,
+                `td-${r}${c}`,
+                false,
+                true,
+                state.content[r][c].innerHTML,
+                {
+                    type: 'input',
+                    func: e => {
+                        state.content[r][c].innerHTML = e.target.textContent;
+                        localStorage.setItem('savedState', JSON.stringify(state));
+                        createTableCode(StateMachine.state, $code_tableCode);
+                    },
+                }
+            );
         }
-
-        for (let r = 0; r < table.rows; r++) {
-            createElement('tbody', `editor-table-${index}`, `table-body-${index}`);
-            createElement('tr', `table-body-${index}`, `table-row-${r}`);
-
-            for (let c = 0; c < table.columns; c++) {
-                createElement(
-                    'td',
-                    `table-row-${r}`,
-                    `td-${r}${c}`,
-                    false,
-                    true,
-                    table.content[r][c].innerHTML,
-                    {
-                        type: 'input',
-                        func: e => {
-                            state.tables[index].content[r][c].innerHTML = e.target.textContent;
-                            localStorage.setItem('savedState', JSON.stringify(state));
-                            createTableCode(StateMachine.state, $code_tableCode);
-                        },
-                    }
-                );
-            }
-        }
-    });
+    }
 };
 
 export default createEditorTable;
--- scripts/createTableCode.js
+++ scripts/createTableCode.js
@@ -6,37 +6,31 @@ const createTableCode = (state, $code_tableCode) => {
     let output = '';
     const indent = amount => '\u00A0'.repeat(amount);
 
-    for (let t = 0; t < state.tables.length; t++) {
-        output += '&lt;table&gt<br>';
+    output += '&lt;table&gt<br>';
 
-        if (state.tables[t].caption) {
-            output += `${indent(2)}&lt;caption&gt${state.tables[t].captionText}&lt;/caption&gt<br>`;
-        }
-
-        if (state.tables[t].header) {
-            output += `${indent(2)}&lt;thead&gt<br>${indent(4)}&lt;tr&gt<br>`;
-            for (let h = 0; h < state.tables[t].columns; h++) {
-                output += `${indent(6)}&lt;th&gt${
-                    state.tables[t].headerContent[h].innerHTML
-                }&lt;/th&gt<br>`;
-            }
-            output += `${indent(4)}&lt;/tr&gt<br>${indent(2)}&lt;/thead&gt<br>`;
-        }
-
-        output += `${indent(2)}&lt;tbody&gt<br>`;
-        for (let i = 0; i < state.tables[t].rows; i++) {
-            output += `${indent(4)}&lt;tr&gt<br>`;
-
-            for (let p = 0; p < state.tables[t].columns; p++) {
-                output += `${indent(6)}&lt;td&gt${
-                    state.tables[t].content[i][p].innerHTML
-                }&lt;/td&gt<br>`;
-            }
-            output += `${indent(4)}&lt;/tr&gt<br>`;
-        }
-        output += `${indent(2)}&lt;/tbody&gt<br>`;
-        output += `&lt;/table&gt<br>`;
+    if (state.caption) {
+        output += `${indent(2)}&lt;caption&gt${state.captionText}&lt;/caption&gt<br>`;
     }
+
+    if (state.header) {
+        output += `${indent(2)}&lt;thead&gt<br>${indent(4)}&lt;tr&gt<br>`;
+        for (let h = 0; h < state.columns; h++) {
+            output += `${indent(6)}&lt;th&gt${state.headerContent[h].innerHTML}&lt;/th&gt<br>`;
+        }
+        output += `${indent(4)}&lt;/tr&gt<br>${indent(2)}&lt;/thead&gt<br>`;
+    }
+
+    output += `${indent(2)}&lt;tbody&gt<br>`;
+    for (let i = 0; i < state.rows; i++) {
+        output += `${indent(4)}&lt;tr&gt<br>`;
+
+        for (let p = 0; p < state.columns; p++) {
+            output += `${indent(6)}&lt;td&gt${state.content[i][p].innerHTML}&lt;/td&gt<br>`;
+        }
+        output += `${indent(4)}&lt;/tr&gt<br>`;
+    }
+    output += `${indent(2)}&lt;/tbody&gt<br>`;
+    output += `&lt;/table&gt<br>`;
 
     $code_tableCode.innerHTML = output;
 };
