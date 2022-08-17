a44d4bd0866a02e2d42b3e1b910baaff7e6b0026
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-08-17 00:30:22 -0500

    styled preview table

--- src/scripts/editor/utl/createElement.js
+++ src/scripts/editor/utl/createElement.js
@@ -1,104 +1,104 @@
 const createElement = (
-    props = {
-        type,
-        id,
-        parent,
-        innerHTML,
-        inputProps,
-        eventObject,
-        attrs,
-    }
+  props = {
+    type,
+    id,
+    parent,
+    innerHTML,
+    inputProps,
+    eventObject,
+    attrs,
+  }
 ) => {
-    let { type, id, parent, innerHTML, inputProps, eventObject, attrs } = props;
+  let { type, id, parent, innerHTML, inputProps, eventObject, attrs } = props;
 
-    let inputContainer;
+  let inputContainer;
 
-    //TODO bug somewhere causing returns with valid prop types
-    if (!type || !parent) {
-        return;
+  //TODO bug somewhere causing returns with valid prop types
+  if (!type || !parent) {
+    return;
+  }
+
+  if (typeof parent === 'string') {
+    parent = document.getElementById(parent);
+  }
+
+  let ele = document.createElement(type);
+
+  if (type === 'input' || type === 'textarea') {
+    if (inputProps.checked) {
+      ele.checked = inputProps.checked;
     }
 
-    if (typeof parent === 'string') {
-        parent = document.getElementById(parent);
+    if (type === 'input') {
+      ele.type = inputProps.type;
+
+      if (inputProps.value) {
+        ele.setAttribute('value', inputProps.value);
+      }
     }
 
-    let ele = document.createElement(type);
-
-    if (type === 'input' || type === 'textarea') {
-        if (inputProps.checked) {
-            ele.checked = inputProps.checked;
-        }
-
-        if (type === 'input') {
-            ele.type = inputProps.type;
-
-            if (inputProps.value) {
-                ele.setAttribute('value', inputProps.value);
-            }
-        }
-
-        if (inputProps.container) {
-            inputContainer = document.createElement(inputProps.container);
-            if (inputProps.containerClass) {
-                inputContainer.classList.add(inputProps.containerClass);
-            }
-        } else {
-            inputContainer = document.createElement('div');
-            inputContainer.classList.add(`${props.id}-container`);
-        }
-
-        if (inputProps.name) {
-            ele.setAttribute('name', inputProps.name);
-        }
-
-        if (inputProps.label && inputProps.for) {
-            const label = document.createElement('label');
-            label.setAttribute('for', inputProps.for);
-            label.innerHTML = inputProps.label;
-            inputContainer.appendChild(label);
-            parent.appendChild(inputContainer);
-            parent = inputContainer;
-        }
-
-        if (inputProps.min) {
-            ele.setAttribute('min', inputProps.min);
-        }
-
-        if (inputProps.max) {
-            ele.setAttribute('max', inputProps.max);
-        }
-
-        if (inputProps.step) {
-            ele.setAttribute('step', inputProps.step);
-        }
+    if (inputProps.container) {
+      inputContainer = document.createElement(inputProps.container);
+      if (inputProps.containerClass) {
+        inputContainer.classList.add(inputProps.containerClass);
+      }
+    } else {
+      inputContainer = document.createElement('div');
+      inputContainer.classList.add(`${props.id}-container`);
     }
 
-    if (id) {
-        ele.setAttribute('id', `${id}`);
+    if (inputProps.name) {
+      ele.setAttribute('name', inputProps.name);
     }
 
-    if (attrs) {
-        for (let a = 0; a < attrs.length; a++) {
-            ele.setAttribute(attrs[a].attr, attrs[a].value);
-
-            // Hacky fix for disabled=false evaluating to true on HTML elements.
-            if (attrs[a].attr === 'disabled' && attrs[a].value === 'false') {
-                ele.removeAttribute('disabled');
-            }
-        }
+    if (inputProps.label && inputProps.for) {
+      const label = document.createElement('label');
+      label.setAttribute('for', inputProps.for);
+      label.innerHTML = inputProps.label;
+      inputContainer.appendChild(label);
+      parent.appendChild(inputContainer);
+      parent = inputContainer;
     }
 
-    if (innerHTML) {
-        ele.innerHTML = innerHTML;
+    if (inputProps.min) {
+      ele.setAttribute('min', inputProps.min);
     }
 
-    if (eventObject) {
-        ele.addEventListener(eventObject.listener, e => {
-            eventObject.func(e);
-        });
+    if (inputProps.max) {
+      ele.setAttribute('max', inputProps.max);
     }
 
-    parent.appendChild(ele);
+    if (inputProps.step) {
+      ele.setAttribute('step', inputProps.step);
+    }
+  }
+
+  if (id) {
+    ele.setAttribute('id', `${id}`);
+  }
+
+  if (attrs) {
+    for (let a = 0; a < attrs.length; a++) {
+      ele.setAttribute(attrs[a].attr, attrs[a].value);
+
+      // Hacky fix for disabled=false evaluating to true on HTML elements.
+      if (attrs[a].attr === 'disabled' && attrs[a].value === 'false') {
+        ele.removeAttribute('disabled');
+      }
+    }
+  }
+
+  if (innerHTML) {
+    ele.innerHTML = innerHTML;
+  }
+
+  if (eventObject) {
+    ele.addEventListener(eventObject.listener, e => {
+      eventObject.func(e);
+    });
+  }
+
+  parent.appendChild(ele);
 };
 
 export default createElement;
